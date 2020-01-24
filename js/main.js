864ff22f066628303e9df5ca517fb7c21985ab38
suzuka92 <suzukasato.job@gmail.com>
2020-01-24 23:09:20 +0900

    おみくじアプリを作成 ・HTML ・CSS ・JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+{
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', () => {
+        const results = ['大吉','中吉','小吉','凶','😇'];
+
+        btn.textContent = results[Math.floor(Math.random() * results.length)];
+     });
+}
